03b9fffc33d83ad963aa6e0fb067955dfecd6b1d
ParkJuHyeok <hyeok5596@naver.com>
2023-01-16 21:52:46 +0900

    init: 초기 개발 환경 구축

---
+++ src/test/java/com/project/anstagram/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.anstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
